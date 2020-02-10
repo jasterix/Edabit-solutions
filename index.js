f69689f1fa2207c09f029b910022b5231e5a6ba6
Jasterix <ljcalixte@gmail.com>
2020-02-10 04:59:31 +0000

    sum digits

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const sum = (a, b)=> {
   return a + b
 }
 
-sum(3, 2)
+// sum(3, 2)
+
+const sumDigits =(num)=> {
+  let numArr = num.toString().split("").map(Number)
+  let result = numArr.reduce((total, current)=> {
+    return total+current
+  })
+  return result
+}
+
+sumDigits(184)
